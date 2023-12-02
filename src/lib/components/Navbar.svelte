f09758a220b5b7b1c4b5a1d39286c7905dbba023
Saige Cross <saigecrossdev@gmail.com>
2023-12-01 18:05:18 -0600

    Added icons w/ links

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,12 +1,33 @@
 <script lang="ts">
 	import { writable } from 'svelte/store';
 	import { scrolledSection } from '$lib/stores';
+	import Github from '$lib/assets/github.svg';
+	import Twitter from '$lib/assets/twitter.svg';
+	import LinkedIn from '$lib/assets/linkedin.svg';
 
 	const pages = writable([
 		{ name: 'Home', id: 'home' },
 		{ name: 'Projects', id: 'projects' },
 		{ name: 'About', id: 'about' },
 		{ name: 'Contact', id: 'contact' }
+	]);
+
+	const socials = writable([
+		{
+			name: 'Github',
+			url: 'https://github.com/saige243',
+			icon: Github
+		},
+		{
+			name: 'Twitter',
+			url: 'https://twitter.com/The_Commodore0',
+			icon: Twitter
+		},
+		{
+			name: 'LinkedIn',
+			url: 'https://www.linkedin.com/in/saige-cross-8b3976241/',
+			icon: LinkedIn
+		}
 	]);
 
 	$: {
@@ -24,7 +45,7 @@
 	}
 </script>
 
-<navbar class="hidden sm:flex sm:flex-col sm:px-4 sm:text-center">
+<navbar class="hidden sm:flex sm:flex-col sm:px-4 sm:text-center justify-between">
 	<div class="w-[100px]">
 		{#each $pages as { name, id }}
 			<button
@@ -34,6 +55,17 @@
 			>
 				{name}
 			</button>
+		{/each}
+	</div>
+	<div class="flex flex-col place-items-center w-[100px] pb-24">
+		{#each $socials as { name, url, icon }}
+			<a
+				href={url}
+				target="_blank"
+				class="py-4 text-gray-800 hover:bg-gray-200 hover:text-gray-900"
+			>
+				<img src={icon} alt={name} />
+			</a>
 		{/each}
 	</div>
 </navbar>
