e9595853ea9caf684054f87fa19d1233632ae654
Saige Cross <saigecrossdev@gmail.com>
2023-12-02 08:59:56 -0600

    fixed toggle

--- src/lib/components/Toggle.svelte
+++ src/lib/components/Toggle.svelte
@@ -1,7 +1,5 @@
 <script>
 	import { theme } from '$lib/theme';
-	import Moon from '$lib/assets/moon.svg';
-	import Sun from '$lib/assets/sun.svg';
 
 	let isDarkMode = $theme === 'dark';
 
@@ -15,15 +13,17 @@
 	<label class="relative inline-flex items-center cursor-pointer">
 		<input type="checkbox" bind:checked={isDarkMode} class="sr-only peer" on:change={toggleTheme} />
 		<div
-			class="w-11 h-6 bg-gray-300 peer-focus:outline-none rounded-full peer dark:bg-gray-900 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-900"
-		></div>
-		<span class="ms-1 text-sm font-medium text-gray-900 dark:text-gray-300">
-			{#if isDarkMode}
-				<img src={Sun} alt="sun" class="w-4 h-4" />
-			{/if}
-			{#if !isDarkMode}
-				<img src={Moon} alt="moon" class="w-4 h-4" />
-			{/if}
-		</span>
+			class="w-12 h-6 bg-gray-300 peer-focus:outline-none rounded-full peer dark:bg-gray-900 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[3.5px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-900"
+		>
+			<div class="mb-px">
+				<span class="ms-1 text-base text-gray-900 dark:text-gray-300">
+					{#if isDarkMode}
+						<span class="w-4 h-4 text-lg pr-2">🌞</span>
+					{:else}
+						<span class="w-4 h-4 text-lg pl-[22px]">🌚</span>
+					{/if}
+				</span>
+			</div>
+		</div>
 	</label>
 </div>
