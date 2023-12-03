9853bc618de71a0234d4744b09c4b7232c168be1
Saige Cross <saigecrossdev@gmail.com>
2023-12-03 13:48:07 -0600

    spin effect working

--- src/lib/components/TechBadge.svelte
+++ src/lib/components/TechBadge.svelte
@@ -3,8 +3,10 @@
 </script>
 
 {#each stackData as item}
-	<a href={item.link} class="flex flex-wrap justify-center p-4">
-		<div class="flex items-center justify-center w-20 h-20 bg-slate-200 shadow-xl rounded-full">
+	<a href={item.link} class="flex flex-wrap justify-center p-4 group">
+		<div
+			class="relative flex items-center justify-center w-24 h-24 bg-slate-200 shadow-xl rounded-full hover:animate-spin"
+		>
 			<img src={item.image} alt={item.title} class="w-12 h-12" />
 		</div>
 	</a>
