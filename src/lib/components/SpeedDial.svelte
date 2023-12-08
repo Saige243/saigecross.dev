906fbefdef530fcea921ee454f34a816f2c9e3db
Saige Cross <saigecrossdev@gmail.com>
2023-12-08 16:53:58 -0600

    Added speedial

---
+++ src/lib/components/SpeedDial.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { SpeedDial, SpeedDialButton } from 'flowbite-svelte';
+	import { ShareNodesSolid, PrintSolid, DownloadSolid, FileCopySolid } from 'flowbite-svelte-icons';
+</script>
+
+<SpeedDial defaultClass="absolute sm:hidden end-6 bottom-6 bg-blue-400 rounded-full p-4 z-10">
+	<SpeedDialButton name="Share">
+		<ShareNodesSolid class="w-5 h-5" />
+	</SpeedDialButton>
+	<SpeedDialButton name="Print">
+		<PrintSolid class="w-5 h-5" />
+	</SpeedDialButton>
+	<SpeedDialButton name="Download">
+		<DownloadSolid class="w-5 h-5" />
+	</SpeedDialButton>
+</SpeedDial>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import '../app.css';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import SpeedDial from '$lib/components/SpeedDial.svelte';
 </script>
 
 <div class="flex w-screen h-screen bg-blue-200 dark:bg-primary lg:px-30 2xl:px-48">
 	<Navbar />
+	<SpeedDial />
 	<slot />
 </div>
 
