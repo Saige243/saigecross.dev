817b27a7d7332aeaa46725516d0cdd60c0a56227
Saige Cross <saigecrossdev@gmail.com>
2025-09-03 10:20:31 -0500

    fix stale svelte-kit

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DQlq6LBX.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/CEZKP6rH.js","_app/immutable/chunks/DaoAKokp.js","_app/immutable/chunks/DAP4otT_.js"];
+export const imports = ["_app/immutable/nodes/1.Dm0ETXYh.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/CEZKP6rH.js","_app/immutable/chunks/DaoAKokp.js","_app/immutable/chunks/B4R7MhLh.js"];
 export const stylesheets = [];
 export const fonts = [];
