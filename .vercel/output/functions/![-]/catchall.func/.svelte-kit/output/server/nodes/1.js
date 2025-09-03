aed320283d008f7559d1c3546e5b22212cfbabdb
Saige Cross <saigecrossdev@gmail.com>
2025-09-03 10:08:24 -0500

    fix vercel deployment

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.buLop72R.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/CEZKP6rH.js","_app/immutable/chunks/DaoAKokp.js","_app/immutable/chunks/C5VmSe3e.js"];
+export const imports = ["_app/immutable/nodes/1.9WCrytC3.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/CEZKP6rH.js","_app/immutable/chunks/DaoAKokp.js","_app/immutable/chunks/Bn_ibBJl.js"];
 export const stylesheets = [];
 export const fonts = [];
--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/![-]/catchall.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["scfavicon.png","scfavicon.svg"]),
 	mimeTypes: {".png":"image/png",".svg":"image/svg+xml"},
 	_: {
-		client: {start:"_app/immutable/entry/start.B24zmgg-.js",app:"_app/immutable/entry/app.BRKF2uxG.js",imports:["_app/immutable/entry/start.B24zmgg-.js","_app/immutable/chunks/C5VmSe3e.js","_app/immutable/chunks/DaoAKokp.js","_app/immutable/entry/app.BRKF2uxG.js","_app/immutable/chunks/DaoAKokp.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/DNmT_gaO.js","_app/immutable/chunks/BLz1e_bf.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.B45_cdHd.js",app:"_app/immutable/entry/app.BSIcYP3i.js",imports:["_app/immutable/entry/start.B45_cdHd.js","_app/immutable/chunks/Bn_ibBJl.js","_app/immutable/chunks/DaoAKokp.js","_app/immutable/entry/app.BSIcYP3i.js","_app/immutable/chunks/DaoAKokp.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/DNmT_gaO.js","_app/immutable/chunks/BLz1e_bf.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js')),
